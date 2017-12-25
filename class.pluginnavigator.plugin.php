6dc7ab12bcddc44e0b0a5fe92ca48602382c3e04
R-J <robin.jurinka@gmx.net>
2017-12-25 15:38:08 +0100

    Create links, using button group

--- class.pluginnavigator.plugin.php
+++ class.pluginnavigator.plugin.php
@@ -22,6 +22,42 @@ class PluginNavigatorPlugin extends Gdn_Plugin {
      * @return void
      */
     public function settingsController_beforeAddonList_handler($sender, $args) {
-decho($args('AvailableAddons'));
+        $keys = [];
+        foreach ($args['AvailableAddons'] as $addon) {
+            $keys[] = $addon['Key'];
+        }
+        sort($keys);
+
+        $lastLetter = '';
+        /*
+        echo '<style>.PluginNavigatorList .Button{display:inline;list-style-type:none;margin:0 0.1em;padding:0.5em 0.9em;min-width:0}.PluginNavigatorList{padding:0;margin-top:1em}</style>';
+        echo '<ul class="PluginNavigatorList">';
+        foreach ($keys as $addonKey) {
+            $linkLetter = substr($addonKey, 0, 1);
+            if ($linkLetter !== $lastLetter) {
+                echo sprintf(
+                    '<li class="Button"><a href="#%s-addon">%s</a></li>',
+                    $addonKey,
+                    $linkLetter
+                );
+            }
+            $lastLetter = $linkLetter;
+        }
+        echo '</ul>';
+        */
+        echo '<style>.PluginNavigatorList{text-transform:uppercase;padding:0;margin-top:1em}.PluginNavigatorList .btn{margin:0 0.1em;padding:0.5em 0.9em;min-width:0}</style>';
+        echo '<div class="toolbar PluginNavigatorList"><div class="btn-group">';
+        foreach ($keys as $addonKey) {
+            $linkLetter = substr($addonKey, 0, 1);
+            if ($linkLetter !== $lastLetter) {
+                echo sprintf(
+                    '<a href="#%1$s-addon" class="btn btn-secondary" accesskey="%2$s">%2$s</a>',
+                    $addonKey,
+                    $linkLetter
+                );
+            }
+            $lastLetter = $linkLetter;
+        }
+        echo '</div></div>';
     }
 }
